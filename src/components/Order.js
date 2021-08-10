3cda13679e61b4e59f81e35e9555f63502082df3
Inokcode <regilearn@gmail.com>
2021-08-10 14:42:46 +0530

    Animation routes added

--- src/components/Order.js
+++ src/components/Order.js
@@ -18,6 +18,10 @@ const containerVariants = {
       staggerChildren: 0.4,
     },
   },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
+  },
 };
 
 const childVariants = {
@@ -30,22 +34,25 @@ const childVariants = {
 };
 
 const Order = ({ pizza }) => {
-  const [showTitle, setShowTitle] = useState(true);
-  setTimeout(() => {
-    setShowTitle(false);
-  }, 4000);
+  // const [showTitle, setShowTitle] = useState(true);
+  // setTimeout(() => {
+  //   setShowTitle(false);
+  // }, 4000);
   return (
     <motion.div
       className="container order"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
+      exit="exit"
     >
-      <AnimatePresence>
+      {/* <AnimatePresence>
         {showTitle && (
           <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>
         )}
-      </AnimatePresence>
+      </AnimatePresence> */}
+
+      <h2>Thank you for your order :)</h2>
 
       <motion.p variants={childVariants}>
         You ordered a {pizza.base} pizza with:
