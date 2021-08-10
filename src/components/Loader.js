c24d3936d36843ab657a9e4f623875ee8a70e3e2
Inokcode <regilearn@gmail.com>
2021-08-10 21:34:53 +0530

    useCycle added

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -39,7 +39,7 @@ const Loader = () => {
         variants={loaderVariants}
         animate={animation}
       ></motion.div>
-      <div onClick={() => cycleAnimation()}>Cycle Loader</div>
+      <div onClick={() => cycleAnimation()}>Cycle Loader click</div>
     </>
   );
 };
